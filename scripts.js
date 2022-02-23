e7ac282335c24cc0ef1004b1b369e054cbf45b38
otrachea <francesca.ho459@gmail.com>
2022-02-23 04:09:14 +0000

    Can set colour of value on bar and set bar spacing

--- scripts.js
+++ scripts.js
@@ -1,10 +1,44 @@
+function getyMax(data, options) {
+  let max = ('yMax' in options) ? options.yMax : Math.max(...data);
+  return max;
+}
+
+function setBarColor(options) {
+  ('barColor' in options) ?
+    $('.bar').css('background-color', `#${options.barColor}`) :
+    $('.bar').css('background-color', `#C297B8`);
+}
+
+function setBarLabelPosition(options) {
+  if ('barLabelPosition' in options) {
+    if (options.barLabelPosition === 'center') {
+      $('.bar-label').css('top', `calc(50% - 0.5em)`);
+    } else if (options.barLabelPosition === 'bottom') {
+      $('.bar-label').css('top', `calc(100% - 1em)`);
+    } else { // options.barLabelPosition === 'top'
+      $('.bar-label').css('top', 0);
+    }
+  }
+}
+
+function setBarLabelColor(options) {
+  ('barLabelColor' in options) ?
+    $('.bar-label').css('color', `#${options.barLabelColor}`) :
+    $('.bar-label').css('color', `#000000`);
+}
+
+function setBarSpacing(options) {
+  ('barSpacing' in options) ?
+    $('.bar').css('margin-right', `${options.barSpacing}px`) :
+    $('.bar').css('margin-right', `10px`);
+}
+  
 function drawBarChart(data, options, element) {
 
   let dataPoints = [];
 
   // SETS Y AXIS MAX VALUE
-  let max;
-  ('yMax' in options) ? max = options.yMax : max = Math.max(...data);
+  let max = getyMax(data, options);
 
   // DRAWS ACTUAL BARS
   data.forEach((dataPoint, index) => {
@@ -20,28 +54,16 @@ function drawBarChart(data, options, element) {
   $('.bar').css('width', `calc(400px / ${data.length})`);
 
   // SETS COLOUR OF BAR
-  if ('barColor' in options) {
-    $('.bar').css('background-color', `#${options.barColor}`)
-  } else {
-    $('.bar').css('background-color', `#C297B8`);
-  }
+  setBarColor(options);
 
   // SETS BAR VALUE LABEL POSITION
-  if ('barLabelPosition' in options) {
-    if (options.barLabelPosition === 'center') {
-      $('.bar-label').css('top', `calc(50% - 0.5em)`);
-    } else if (options.barLabelPosition === 'bottom') {
-      $('.bar-label').css('top', `calc(100% - 1em)`);
-    } else { // options.barLabelPosition === 'top'
-      $('.bar-label').css('top', 0);
-    }
-  }
+  setBarLabelPosition(options);
 
   // SETS BAR VALUE LABEL COLOUR
-
+  setBarLabelColor(options);
 
   // SETS BAR SPACING
-
+  setBarSpacing(options);
 
   // SET BAR CHART AXES NAMES
 
@@ -59,6 +81,10 @@ function drawBarChart(data, options, element) {
 
 drawBarChart([1, 2, 1.85, 4, 7], {
   'yMax': 7,
-  'xLabels': ['a', 'b', 'c', 'd', 'e'], 'barLabelPosition': 'top'
+  'xLabels': ['a', 'b', 'c', 'd', 'e'],
+  'barColor': 'DE89BE',
+  'barLabelPosition': 'center',
+  'barSpacing': 50,
+  // 'barLabelColor': '40434E'
 }, $('div'));
 // drawBarChart([1, 2, 0.5, 4, 7], {}, $('div'));
