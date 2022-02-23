695a017d316bf3f044109223f445a0b1b4cda8c1
otrachea <francesca.ho459@gmail.com>
2022-02-23 02:00:43 +0000

    Chart displays value of each bar on bar with customizable position

--- scripts.js
+++ scripts.js
@@ -1,24 +1,64 @@
 function drawBarChart(data, options, element) {
 
-  let userOptions = Object.keys(options);
+  let dataPoints = [];
 
-  let max = Math.max(...data);
+  // SETS Y AXIS MAX VALUE
+  let max;
+  ('yMax' in options) ? max = options.yMax : max = Math.max(...data);
 
   // DRAWS ACTUAL BARS
   data.forEach((dataPoint, index) => {
-    element.append($('<div></div>')
+    if ('xLabels' in options) {
+      dataPoints.push({ 'label': options.xLabels[index], 'value': dataPoint });
+    } else {
+      dataPoints.push({ 'label': dataPoint, 'value': dataPoint });
+    }
+    element.append($(`<div><span class="bar-label">${dataPoints[index].value}</span></div>`)
       .addClass('bar')
       .css('height', `calc(600px * (${dataPoint} / ${max}))`));
   });
   $('.bar').css('width', `calc(400px / ${data.length})`);
 
   // SETS COLOUR OF BAR
-  if (userOptions.includes('barColor')) {
-    $('.bar').css('background-color', `#${barColor}`)
+  if ('barColor' in options) {
+    $('.bar').css('background-color', `#${options.barColor}`)
   } else {
-    $('.bar').css('background-color', `#40434E`);
+    $('.bar').css('background-color', `#C297B8`);
   }
 
+  // SETS BAR VALUE LABEL POSITION
+  if ('barLabelPosition' in options) {
+    if (options.barLabelPosition === 'center') {
+      $('.bar-label').css('top', `calc(50% - 0.5em)`);
+    } else if (options.barLabelPosition === 'bottom') {
+      $('.bar-label').css('top', `calc(100% - 1em)`);
+    } else { // options.barLabelPosition === 'top'
+      $('.bar-label').css('top', 0);
+    }
+  }
+
+  // SETS BAR VALUE LABEL COLOUR
+
+
+  // SETS BAR SPACING
+
+
+  // SET BAR CHART AXES NAMES
+
+
+
+
+  // SETS CHART TITLE
+
+
+  // SETS CHART TITLE FONT SIZE
+
+
+  // SETS CHART TITLE FONT COLOUR
 }
 
-drawBarChart([1, 2, 0.5, 4, 7], {}, $('div'));
+drawBarChart([1, 2, 1.85, 4, 7], {
+  'yMax': 7,
+  'xLabels': ['a', 'b', 'c', 'd', 'e'], 'barLabelPosition': 'top'
+}, $('div'));
+// drawBarChart([1, 2, 0.5, 4, 7], {}, $('div'));
