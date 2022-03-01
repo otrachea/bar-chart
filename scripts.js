d514a99e440ccb102294b19f45a4cb53ecc5eaf0
otrachea <francesca.ho459@gmail.com>
2022-03-01 21:27:30 +0000

    drawBarChart can successfully draw only 1 graph at a time

--- scripts.js
+++ scripts.js
@@ -1,44 +1,100 @@
 function getyMax(data, options) {
-  let max = ('yMax' in options) ? options.yMax : Math.max(...data);
+  let max;
+  ('yMax' in options && options.yMax !== null) ?
+    max = options.yMax :
+    max = Math.max(...data);
+  
+  // let max = ('yMax' in options) ? (options.yMax) : Math.max(...data);
   return max;
 }
 
 function setBarColor(options) {
-  ('barColor' in options) ?
+  ('barColor' in options && options.barColor !== null) ?
     $('.bar').css('background-color', `#${options.barColor}`) :
     $('.bar').css('background-color', `#C297B8`);
 }
 
 function setBarLabelPosition(options) {
-  if ('barLabelPosition' in options) {
+  if ('barLabelPosition' in options && options.barLabelPosition !== null) {
     if (options.barLabelPosition === 'center') {
       $('.bar-label').css('top', `calc(50% - 0.5em)`);
     } else if (options.barLabelPosition === 'bottom') {
-      $('.bar-label').css('top', `calc(100% - 1em)`);
+      $('.bar-label').css('top', `calc(100% - 1.5em)`);
     } else { // options.barLabelPosition === 'top'
-      $('.bar-label').css('top', 0);
+      $('.bar-label').css('top', '0.5em');
     }
+  } else {
+    
+    $('.bar-label').css('top', '0.5em');
   }
 }
 
 function setBarLabelColor(options) {
-  ('barLabelColor' in options) ?
+  ('barLabelColor' in options && options.barLabelColor !== null) ?
     $('.bar-label').css('color', `#${options.barLabelColor}`) :
     $('.bar-label').css('color', `#000000`);
 }
 
 function setBarSpacing(options) {
-  ('barSpacing' in options) ?
-    $('.bar').css('margin-right', `${options.barSpacing}px`) :
-    $('.bar').css('margin-right', `10px`);
+  if ('barSpacing' in options && options.barSpacing !== null) {
+    $('.bar').css('margin-left', `${options.barSpacing}px`);
+    $('.bar').last().css('margin-right', `${options.barSpacing}px`);
+  } else {
+    $('.bar').css('margin-left', `10px`);
+    $('.bar').last().css('margin-right', '10px');
+  }
 }
-  
+
+function setAxesNames(options) {
+  if ('yAxisName' in options && options.yAxisName !== null) {
+    $(`<div class='y-axis-label'><p>${options.yAxisName}</p></div>`).insertBefore(".graph-container");
+  }
+  if ('xAxisName' in options && options.xAxisName !== null) {
+    $(`<div class='x-axis-label'><p>${options.xAxisName}</p></div>`).insertBefore(".graph-container");
+  }
+}
+
+function setTitleFontSize(options) {
+  if ('titleFontSize' in options && options.titleFontSize !== null) {
+    $('.title > h1').css('font-size', options.titleFontSize);
+  }
+}
+
+function setTitleFontColor(options) {
+  if ('titleFontColor' in options && options.titleFontColor !== null) {
+    $('.title > h1').css('color', `#${options.titleFontColor}`);
+  }
+}
+
+function setTitle(options) {
+  if ('title' in options && options.title !== null) {
+    $(`<div class='title'><h1>${options.title}</h1></div>`).insertBefore(".y-axis-label");
+  } else if (options.title === null) {
+    $(`<div class='title'><h1>Title</h1></div>`).insertBefore(".y-axis-label");
+  } else {
+    $(`<div class='title'></div>`).insertBefore(".y-axis-label");
+  }
+  setTitleFontSize(options);
+  setTitleFontColor(options);
+}
+
+function drawMarkers(options, max) {
+  $(`<div class='markers-container'></div>`).insertBefore('.graph-container');
+  let temp;
+  for (let i = 4; i >= 0; i--) {
+    $('.markers-container').append(`<div class='marker'>${max * (i / 4)}</div>`);
+    $(`.marker:nth-child(${5 - i})`).css('margin-right', '0.5em');
+  }
+}
+
 function drawBarChart(data, options, element) {
 
   let dataPoints = [];
 
   // SETS Y AXIS MAX VALUE
   let max = getyMax(data, options);
+
+  element.append("<div class='graph-container'></div>");
 
   // DRAWS ACTUAL BARS
   data.forEach((dataPoint, index) => {
@@ -47,10 +103,12 @@ function drawBarChart(data, options, element) {
     } else {
       dataPoints.push({ 'label': dataPoint, 'value': dataPoint });
     }
-    element.append($(`<div><span class="bar-label">${dataPoints[index].value}</span></div>`)
+    $('.graph-container').append($(`<div><span class="bar-label">${dataPoints[index].value}</span></div>`)
       .addClass('bar')
       .css('height', `calc(600px * (${dataPoint} / ${max}))`));
   });
+
+  // SETS WIDTH OF BAR
   $('.bar').css('width', `calc(400px / ${data.length})`);
 
   // SETS COLOUR OF BAR
@@ -66,25 +124,26 @@ function drawBarChart(data, options, element) {
   setBarSpacing(options);
 
   // SET BAR CHART AXES NAMES
+  setAxesNames(options);
 
+  // SETS CHART TITLE + FONT SIZE + FONT COLOUR
+  setTitle(options);
 
-
-
-  // SETS CHART TITLE
-
-
-  // SETS CHART TITLE FONT SIZE
-
-
-  // SETS CHART TITLE FONT COLOUR
+  // SETS Y AXIS MARKERS
+  drawMarkers(options, max);
 }
 
-drawBarChart([1, 2, 1.85, 4, 7], {
-  'yMax': 7,
+drawBarChart([16, 2, 1.85, 4, 7, 1, 16], {
+  'yMax': null,
   'xLabels': ['a', 'b', 'c', 'd', 'e'],
   'barColor': 'DE89BE',
   'barLabelPosition': 'center',
-  'barSpacing': 50,
-  // 'barLabelColor': '40434E'
-}, $('div'));
-// drawBarChart([1, 2, 0.5, 4, 7], {}, $('div'));
+  'barSpacing': 20,
+  'barLabelColor': '40434E',
+  'xAxisName': 'X axis',
+  'yAxisName': 'Y axis humina humina aaaeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  'title': 'Fuck this graph',
+  'titleFontSize': '30px',
+  'titleFontColor': null, //344055
+}, $('#container1'));
+drawBarChart([1, 2, 0.5, 4, 7], {}, $('#container2'));
